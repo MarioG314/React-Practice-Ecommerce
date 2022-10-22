f1794378e270a3f5b56cda835b2da5b7a211c848
Mario Garcia <garciamd91@gmail.com>
2022-10-22 12:21:29 -0300

    -- Listo products con fetch.  -- Hago la grilla con map.

--- src/components/GroupCards.jsx
+++ src/components/GroupCards.jsx
@@ -1,13 +1,10 @@
 import React from 'react'
-import CardBasic from './Card'
 
 
 function GroupCards() {
   return (
     <>
-      <CardBasic />
-      <CardBasic />
-      <CardBasic />
+    
     </>
   );
 }
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -2,30 +2,42 @@ import React, { useEffect,useState } from "react";
 import Navbar from "../components/Navbar";
 import Landing from "../components/Landing";
 import Footer from "../components/Footer";
-import { Container } from "react-bootstrap";
+import { Card, Container, Button, Row } from "react-bootstrap";
 
 const Main = () => {
   const [data, setData] = useState([])
   useEffect(() => {
-    fetch("https://fakestoreapi.com/products/3")
+    fetch("https://fakestoreapi.com/products")
       .then((res) => res.json())
       .then((json) => setData(json));
 
-      console.log('useEffect')
   }, [])
   
   return (
     <>
-    {console.log('comienzo a cargar la UI')}
-      <Container fluid className="px-0 d-flex flex-column min-vh-100">
+      <Container fluid className="px-0 d-flex flex-column min-vh-100 ">
         <Navbar />
         <Landing />
-        <div>{data.title}</div>
-        <div>{data.price}</div>
-        <div>{data.description}</div>
+        <Row xs={1} md={2} className="g-4 py-3 justify-content-center">
+          {data.map((d, i) => (
+            <Card className="mx-2" style={{ width: "13rem" }}>
+              <Card.Img variant="top" src={d.image} />
+              <Card.Body>
+                <Card.Title>
+                  <div>{d.title}</div>
+                </Card.Title>
+                <Card.Text></Card.Text>
+              </Card.Body>
+
+              <Card.Footer className="d-flex justify-content-between align-items-center">
+                <div>{d.price}</div>
+                <Button variant="primary">Comprar</Button>
+              </Card.Footer>
+            </Card>
+          ))}
+        </Row>
         <Footer />
       </Container>
-      {console.log('termino de cargar la UI')}
     </>
   );
 };
