6ff775971ddcf1adf38cf5f9e86ffbb8cd6669d0
Mario Garcia <garciamd91@gmail.com>
2022-10-23 22:44:57 -0300

    -- Fix con class max-width-100%

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import {Row} from 'react-bootstrap'
 import Product from './Product'
+import '../css/ProductList.css'
 
 
 const ProductList = (props) => {
   return (
     <div>
-      <Row xs={1} md={2} className="g-4 py-3 justify-content-center">
+      <Row xs={1} md={2} className="fix g-4 py-3 justify-content-center">
         {props.data.map((d, i) => (
           <Product d={d} key={i} />
         ))}
